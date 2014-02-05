cdc3cc679b4caf67ee45786ed1d722499593f37a
Robert Sefer <rsefer@gmail.com>
2014-02-04 23:23:23 -0600

    Somehow got devise working, carrier wave working, etc etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ActiveRecord::Base
+	
+	mount_uploader :avatar, AvatarUploader
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   
   has_many :comments, :dependent => :destroy
   has_many :ratings, :dependent => :destroy
   
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  
+  def fullname
+		if !self.firstname.blank? && !self.lastname.blank?
+			self.firstname +  " " + self.lastname
+		else
+			self.email
+		end
+	end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Chicagofood::Application.routes.draw do
   	resources :ratings
   end
 
-  devise_for :users, path_names: { sign_up: 'register' }
+  devise_for :users, path_names: { sign_up: 'register' }, :controllers => { :registrations => 'registrations' }
 	
 	root "welcome#index"
 	
