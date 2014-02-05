fca52399dd3a3276ae5105782cb683bb73ddc849
Robert Sefer <rsefer@gmail.com>
2014-02-05 12:15:01 -0600

    Cleaned up leftovers

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,11 +1,2 @@
 jQuery(document).ready(function($) {
-	
-	$('.datepicker').datepicker({
-		format: 'yyyy-mm-dd'
-	});
-	
-	$('[data-print="true"]').click(function() {
-		window.print();
-	});
-	
 });
