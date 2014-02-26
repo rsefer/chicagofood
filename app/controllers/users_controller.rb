7f0b96954ed259f4f2d0d0fa205877684c8a74f3
Robert Sefer <rsefer@gmail.com>
2014-02-26 11:59:35 -0600

    Added avatars to users page, added link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.all.sort{ |a,b| a.firstname <=> b.firstname }
 
     respond_to do |format|
       format.html
