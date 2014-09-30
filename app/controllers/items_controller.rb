7acbbd57520efd599febf045044a35cf3f5b14c8
Robert Sefer <rsefer@gmail.com>
2014-09-30 14:57:11 -0500

    Display only items for a particular venue

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.where(venue_id: params[:venue_id])
   end
 
   def show
