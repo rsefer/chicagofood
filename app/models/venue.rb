d297a6a410bebd7df8ad0edad038efb2300fc33b
Robert Sefer <rsefer@gmail.com>
2014-12-10 09:28:36 -0600

    Fixes delete error

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,7 +3,6 @@ class Venue < ActiveRecord::Base
 	has_many :ratings, :dependent => :destroy
 	has_many :tries, :dependent => :destroy
 	has_many :items, :dependent => :destroy
-	has_many :eats, :dependent => :destroy
 
 	belongs_to :venuetype
 	belongs_to :neighborhood
