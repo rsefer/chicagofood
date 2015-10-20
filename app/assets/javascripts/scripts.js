91169e53edcafd803058c6abcea22508b04967e0
Robert Sefer <rsefer@gmail.com>
2015-10-20 14:20:45 -0500

    DRYed assets, venue

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -16,10 +16,45 @@ function initialize(latitude, longitude) {
 	map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
 }
 
+function changeRatingStars(whichStars, starNum) {
+	var thisStar = $(whichStars + ' i.fa:eq(' + starNum + ')');
+	thisStar.removeClass().addClass('fa fa-star');
+	thisStar.prevAll(whichStars + ' i.fa').removeClass().addClass('fa fa-star');
+	thisStar.nextAll(whichStars + ' i.fa').removeClass().addClass('fa fa-star-o');
+}
+
 jQuery(document).ready(function($) {
 
 	$('.datepicker').datepicker({
 		format: 'yyyy-mm-dd'
+	});
+
+	var myratingHTML = $('#myrating').html();
+
+	$('#myrating i.fa').click(function() {
+		var thisClickIndex = $('#myrating i.fa').index(this) + 1;
+		$('input#rating_rating').val(thisClickIndex);
+		$('form.edit_rating, form.new_rating').submit();
+		$('form.edit_rating, form.new_rating').bind('ajax:complete', function() {
+			$.get(window.location.pathname + '/rating_average', function(data) {
+				var newRatingIndex = $('#ourrating i.fa').index(data.rating_average);
+				changeRatingStars('#ourrating', data.rating_average - 1);
+			});
+		});
+
+		myratingHTML = $('#myrating').html();
+
+	});
+
+	$('#myrating i.fa').hover(function() {
+		var thisClickIndex = $('#myrating i.fa').index(this);
+		changeRatingStars('#myrating', thisClickIndex);
+	}, function() {
+	});
+
+	$('#totrysubmit').click(function(e) {
+		e.preventDefault();
+		$('form#new_try').submit();
 	});
 
 });
