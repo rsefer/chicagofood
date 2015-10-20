f0e2d3f519b00d861f42a9d09339afac659dc802
Robert Sefer <rsefer@gmail.com>
2015-10-20 14:33:15 -0500

    Asset cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 			:medium => '300x300#',
 			:thumb => '100x100#'
 		},
-		:default_url => '/images/default-avatar.jpg',
+		:default_url => ActionController::Base.helpers.asset_path('default-avatar.jpg'),
 		:storage => :s3,
 		:s3_credentials => "#{Rails.root}/config/aws.yml"
 
