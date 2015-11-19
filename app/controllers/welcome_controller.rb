793a5710ac748ed3fb8e6e6f1e92773e3dbb8ac3
Robert Sefer <rsefer@gmail.com>
2015-11-18 22:14:49 -0600

    Fixed search and sorting throughout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+  helper_method :sort_column, :sort_direction
+
   def index
   	@venuetypes = Venuetype.all
   	@neighborhoods = Neighborhood.all
@@ -9,50 +11,66 @@ class WelcomeController < ApplicationController
   end
 
   def search
+    @searchresults = []
     if params[:q]
-      @searchresults = []
+      searchresultsSet = []
       @original_query = params[:q]
       query = params[:q].downcase.split(' ').map(&:strip).uniq
 
       query.delete('the')
 
-      byob_results = []
+      byob_results = Set.new
       if query.include? 'byob'
         byob = true
         query.delete('byob')
         byob_results = Venue.where(byob: true).all
       end
 
-      n_results = []
-      vtypes_results = []
+      n_results = Set.new
+      vtypes_results = Set.new
       query.each do |qw|
         Neighborhood.where("lower(name) LIKE ?", "%#{qw}%").each do |n|
           n.venues_with_children.each do |v|
-            n_results.push(v)
+            n_results.add(v)
           end
         end
         Venuetype.where("lower(name) LIKE ?", "%#{qw}%").each do |vt|
           vt.venues.each do |v|
-            vtypes_results.push(v)
+            vtypes_results.add(v)
           end
         end
       end
 
       if byob_results.length > 0 and n_results.length > 0 and vtypes_results.length > 0
-        @searchresults = byob_results & n_results & vtypes_results
+        searchresultsSet = byob_results & n_results & vtypes_results
       elsif byob_results.length > 0 and vtypes_results.length > 0
-        @searchresults = byob_results & vtypes_results
+        searchresultsSet = byob_results & vtypes_results
       elsif byob_results.length > 0 and n_results.length > 0
-        @searchresults = byob_results & n_results
+        searchresultsSet = byob_results & n_results
       elsif n_results.length > 0 and vtypes_results.length > 0
-        @searchresults = n_results & vtypes_results
+        searchresultsSet = n_results & vtypes_results
       elsif byob_results.length > 0
-        @searchresults = byob_results
+        searchresultsSet = byob_results
       elsif n_results.length > 0
-        @searchresults = n_results
+        searchresultsSet = n_results
       elsif vtypes_results.length > 0
-        @searchresults = vtypes_results
+        searchresultsSet = vtypes_results
       end
+      
+      searchresultsSet.each do |vs|
+        @searchresults.push(vs)
+      end
+
+      if params[:sort].present? or params[:direction].present?
+        if params[:sort] == 'name'
+          @searchresults = @searchresults.sort_by { |v| v.sortable_name }
+        else
+          @searchresults = @searchresults.sort_by(&:"#{params[:sort]}")
+        end
+      else
+        @searchresults = @searchresults.sort_by { |v| v.sortable_name }
+      end
+      @searchresults = @searchresults.reverse if params[:direction] == 'desc'
 
     end
   end
@@ -91,5 +109,14 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  private
+    def sort_column
+      Venue.column_names.include?(params[:sort]) ? params[:sort] : "name"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
 
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -21,4 +21,34 @@ class Neighborhood < ActiveRecord::Base
     end
   end
 
+  def venues_with_children
+    tempNeighborhoodList = Set.new [self]
+    Neighborhood.where(parent_neighborhood_id: self.id).each do |n|
+      tempNeighborhoodList.add(n)
+      Neighborhood.where(parent_neighborhood_id: n.id).each do |n2|
+        tempNeighborhoodList.add(n2)
+        Neighborhood.where(parent_neighborhood_id: n2.id).each do |n3|
+          tempNeighborhoodList.add(n3)
+          Neighborhood.where(parent_neighborhood_id: n3.id).each do |n4|
+            tempNeighborhoodList.add(n4)
+            Neighborhood.where(parent_neighborhood_id: n4.id).each do |n5|
+              tempNeighborhoodList.add(n5)
+            end
+          end
+        end
+      end
+    end
+    venuesListSet = Set.new
+    tempNeighborhoodList.each do |n|
+      n.venues.each do |v|
+        venuesListSet.add(v)
+      end
+    end
+    venuesList = []
+    venuesListSet.each do |v|
+      venuesList.push(v)
+    end
+    venuesList
+  end
+
 end
