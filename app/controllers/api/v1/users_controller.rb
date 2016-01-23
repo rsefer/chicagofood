a74b007dc08207efc21666fb877733e84e87e3cc
Robert Sefer <rsefer@gmail.com>
2016-01-23 17:38:59 -0600

    Excluded user info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,23 @@
 class Api::V1::UsersController < Api::ApiController
   before_action :set_user, only: [:show]
+  before_action :set_user_excludes
 
   def index
     @users = User.all
-    respond_with @users
+    respond_with @users, :except => @user_excludes
   end
 
   def show
-    render json: @user
+    render :json => @user, :except => @user_excludes
   end
 
   private
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_user_excludes
+      @user_excludes = [:email, :admin, :avatar_file_name, :avatar_updated_at, :city, :state, :street]
     end
 
 end
