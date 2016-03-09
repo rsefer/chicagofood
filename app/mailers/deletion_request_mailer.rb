9829b56a34069b0635c043dd36ea8f38cac8e3cd
Robert Sefer <rsefer@gmail.com>
2016-03-09 16:03:06 -0600

    Changed email to AWS SES

--- app/mailers/deletion_request_mailer.rb
+++ app/mailers/deletion_request_mailer.rb
@@ -18,7 +18,28 @@ class DeletionRequestMailer < ActionMailer::Base
       @requested_link = 'neighborhoods/' + @requested.id.to_s
     end
 
-    mail(to: 'rsefer@gmail.com', subject: 'Deletion Request')
+    email_html_raw = render_to_string(:action => 'deletion_request_email.html', :locals => { :user => @user, :requested => @requested, :requested_text_label => @requested_text_label, :requested_link => @requested_link })
+
+    html_roadie = Roadie::Document.new email_html_raw
+    html_inlined = html_roadie.transform
+
+    ses = Aws::SES::Client.new(region: ENV['AWS_REGION'], credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']))
+    ses.send_email({
+      source: "Chicago Food <chicagofood@seferdesign.com>",
+      destination: {
+        to_addresses: ["chicagofood@seferdesign.com"]
+      },
+      message: {
+        subject: {
+          data: "Deletion Request"
+        },
+        body: {
+          html: {
+            data: html_inlined
+          }
+        }
+      }
+    })
 
   end
 
