8602a2555b74938b7a9c4b43e6fdaa7374653b32
Robert Sefer <rsefer@gmail.com>
2016-04-21 09:49:07 -0500

    Fixed unique list item issue

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,7 +2,7 @@ class ListItem < ActiveRecord::Base
   belongs_to :list
   belongs_to :venue
 
-  validates_uniqueness_of :venue_id, :message => 'Venue already exists in this list.'
+  validates_uniqueness_of :venue_id, scope: :list_id, :message => 'Venue already exists in this list.'
 
   default_scope { order('date DESC') }
 
